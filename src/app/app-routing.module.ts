e6fadeee7dd7157899ec60f8d692bc83e42786ca
Cristopher Estrada <crissin21_01@hotmail.com>
2019-07-26 10:54:03 -0500

    registro y formulario v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { DiagnosticComponent } from './components/diagnostic/diagnostic.component';
+import { TreatmentComponent } from './components/treatment/treatment.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { AboutComponent } from './components/about/about.component';
+import { FaqComponent } from './components/faq/faq.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'diagnostico', component: DiagnosticComponent },
+  { path: 'tratamiento', component: TreatmentComponent },
+  { path: 'login', component: SigninComponent },
+  { path: 'registro', component: SignupComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'faq', component: FaqComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { DiagnosticComponent } from './components/diagnostic/diagnostic.component';
+import { TreatmentComponent } from './components/treatment/treatment.component';
+import { AboutComponent } from './components/about/about.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { FaqComponent } from './components/faq/faq.component';
+import { FirstLetterPipe } from './pipes/first-letter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    DiagnosticComponent,
+    TreatmentComponent,
+    AboutComponent,
+    SignupComponent,
+    SigninComponent,
+    FaqComponent,
+    FirstLetterPipe
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
