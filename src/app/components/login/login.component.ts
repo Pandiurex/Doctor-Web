aec50e5f54c2b085ffa6e5f853b056012eb831bc
fercho97 <fernando.jaime@alumnos.udg.mx>
2019-09-07 21:25:45 -0500

    Conexión y funcionalidad login form con backend

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { 
+  Component, 
+  OnInit,
+  ViewChild,
+  ElementRef
+ } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormArray
+} from '@angular/forms';
+import { Observable } from 'rxjs';
+import { HttpParams, HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { LoginService } from './login.service';
+import {Router} from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent implements OnInit {
+  @ViewChild('cerrar', {static : false}) cerrar: ElementRef;
+  login: FormGroup;
+  private values : HttpParams;
+  
+  constructor(private logServ : LoginService, private http : HttpClient, private router : Router, private toast : ToastrService) { 
+    
+    
+    this.login = new FormGroup({
+            emailOrNickname: new FormControl('', Validators.required),
+            logPassword: new FormControl('', Validators.required),
+          });
+  }
 
   ngOnInit() {
   }
 
+  loginCheck(){
+    console.log(this.login);
+    this.values = new HttpParams()
+    .set('nickOrEmail', this.login.value.emailOrNickname)
+    .set('password', this.login.value.logPassword);
+    this.logServ.checkLogin(this.values).subscribe( (res : any) =>{
+
+    if(res.body.message=="Verificacion"){
+      this.toast.info('Su cuenta aun no se encuentra verificada, favor de verificarla mediante su correo.', 'Cuenta sin verificar');
+    }else{
+    this.toast.success('Bienvenido al sistema Medic Path ' +  res.body.usuario, 'Éxito!');
+    this.cerrar.nativeElement.click();
+    //Pending
+    //Guardar sesion de usuario
+    this.router.navigate(['/home']);
+
+    }
+  }, error =>{
+      console.log("Error", error.error.message);
+      this.toast.error(error.error.message, 'Error');
+  })
+  }
+
+  resetModal(){
+    this.login.reset();
+  }
 }
---
+++ src/app/components/login/login.service.ts
@@ -0,0 +1,25 @@
+import { HttpParams, HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {Router} from '@angular/router';
+@Injectable({
+    providedIn: 'root'
+})
+export class LoginService{
+    _url : string = '';
+    private router: Router;
+    constructor(private _http: HttpClient) {
+        this._url = 'http://localhost:3000/usuarios/login';
+    }
+
+    checkLogin(valores : HttpParams){
+        return this._http.post(this._url,
+            valores.toString(),
+            {
+              headers: new HttpHeaders()
+                .set('Content-Type', 'application/x-www-form-urlencoded'),
+              observe : 'response'
+            },
+          )
+    }
+}
