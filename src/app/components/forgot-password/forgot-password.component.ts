bd097dcd4c04bd82a27fb21daf56e9a9ff7c0976
fercho97 <fernando.jaime@alumnos.udg.mx>
2019-09-10 22:42:48 -0500

    Hotfixes y funcionalidad de recuperación de contraseña con backend

--- src/app/components/forgot-password/forgot-password.component.ts
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { Observable } from 'rxjs';
+import { HttpParams, HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { RecoveryService } from './forgot-password.service';
+import {Router} from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,9 +18,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  constructor() { }
+  values : HttpParams;
+  recovery : FormGroup;
+  constructor(private recServ : RecoveryService, private http : HttpClient, private toastr : ToastrService, private router : Router) { 
+
+    this.recovery = new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$'),
+      ])
+    });
+  }
 
   ngOnInit() {
   }
 
+  sendReset(){
+    this.values = new HttpParams()
+    .set('email', this.recovery.value.email);
+
+    this.recServ.resetRequest(this.values).subscribe((res :any) =>{
+      this.toastr.info("Se ha enviado un correo a la dirección que indico, llegara en un momento", "Enviado");
+      this.router.navigate(['/home']);
+    }, error =>{
+      console.log(error);
+      this.router.navigate(['/home']);
+    })
+    
+  }
 }
---
+++ src/app/components/forgot-password/forgot-password.service.ts
@@ -0,0 +1,26 @@
+import { HttpParams, HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {Router} from '@angular/router';
+@Injectable({
+    providedIn: 'root'
+})
+
+export class RecoveryService{
+    _url : string = '';
+    private router: Router;
+    constructor(private _http: HttpClient) {
+        this._url = 'http://localhost:3000/usuarios/resetPassword';
+    }
+
+    resetRequest(valores : HttpParams){
+        return this._http.post(this._url,
+            valores.toString(),
+            {
+              headers: new HttpHeaders()
+                .set('Content-Type', 'application/x-www-form-urlencoded'),
+              observe : 'response'
+            },
+          )
+    }
+}
