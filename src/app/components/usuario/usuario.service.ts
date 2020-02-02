9a3bcf77808772708c7b59f7e8fa605cc6101274
fercho97 <fernando.jaime@alumnos.udg.mx>
2020-02-02 14:15:16 -0600

    Routes

--- src/app/components/usuario/usuario.service.ts
+++ src/app/components/usuario/usuario.service.ts
@@ -7,12 +7,16 @@ export class UsuarioService {
   _url: string = "";
   _urlIndividual: string = "";
   _urlDoctor: string = "";
+  _urlAllDocs: string = "";
   constructor(private _http: HttpClient) {
     this._url = "https://medicpath.herokuapp.com/usuarios/userlist/";
     //'http://localhost:3000/usuarios/userlist/';
     this._urlIndividual = "https://medicpath.herokuapp.com/usuarios/";
     //'http://localhost:3000/usuarios/'
-    this._urlDoctor = "http://localhost:3000/usuarios/doctor/"
+    this._urlDoctor = "https://medicpath.herokuapp.com/doctor/";
+    //"http://localhost:3000/usuarios/doctor/";
+    this._urlAllDocs = "https://medicpath.herokuapp.com/doctorlist/";
+    //"http://localhost:3000/usuarios/doctorlist/";
 
   }
 
@@ -38,6 +42,16 @@ export class UsuarioService {
 
   getDoctorInfo(hash: any){
     return this._http.get(this._urlDoctor + encodeURIComponent(hash), {
+      headers: new HttpHeaders().set(
+        "Content-Type",
+        "application/x-www-form-urlencoded"
+      ),
+      observe: "response"
+    });
+  }
+
+  getDoctors(){
+    return this._http.get(this._urlAllDocs, {
       headers: new HttpHeaders().set(
         "Content-Type",
         "application/x-www-form-urlencoded"
