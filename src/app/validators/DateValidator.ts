bf8295fe6a20955f0452d2a07bba2e751dc5ba56
fercho97 <fernando.jaime@alumnos.udg.mx>
2020-02-11 21:20:18 -0600

    Fix validador

--- src/app/validators/DateValidator.ts
+++ src/app/validators/DateValidator.ts
@@ -4,17 +4,18 @@ export class DateValidator {
     static isFutureDate(control : AbstractControl){
         let todayDate = moment().format('L');
         let controlDate = moment(control.value).format('L');
-        if(controlDate < todayDate || controlDate != todayDate){
+        if(controlDate < todayDate && controlDate != todayDate){
             return null;
-        }else{
+        }else if(controlDate > todayDate){
             return{'isFutureDate': true}
         }
     }
 
     static noValidAge(control: AbstractControl){
         let enteredDate = moment(control.value).fromNow();
-        console.log(enteredDate);
-        if(enteredDate == "18 years ago" || enteredDate == "17 years ago" || enteredDate == "16 years ago"){
+        let todayDate = moment().format('L');
+        let controlDate = moment(control.value).format('L');
+        if(enteredDate == "18 years ago" || enteredDate == "17 years ago" || enteredDate == "16 years ago" || controlDate > todayDate){
             return null
         }else{
             return{'noValidAge': true}  
